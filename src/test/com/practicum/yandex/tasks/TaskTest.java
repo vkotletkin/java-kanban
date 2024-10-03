dceaf61fc14b9c09a6029e610b2bea45769766b6
Vladislav Kotletkin <drzhkn1@mail.ru>
2024-10-04 00:46:41 +0300

    Added TaskTest prototype

---
+++ src/test/com/practicum/yandex/tasks/TaskTest.java
@@ -0,0 +1,13 @@
+package com.practicum.yandex.tasks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+
+    @Test
+    public void tasksShouldBeEqualsWithEqualUUID() {
+        // Task taskFirst = new Task();
+    }
+}
