00c8ddd2e43307d04e44eb13dda8e4ca2721d219
Vladislav Kotletkin <drzhkn1@mail.ru>
2024-12-10 20:01:58 +0300

    fix: Убрал лишние импорты

--- src/main/com/practicum/yandex/adapters/DurationAdapter.java
+++ src/main/com/practicum/yandex/adapters/DurationAdapter.java
@@ -3,14 +3,9 @@ package com.practicum.yandex.adapters;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import com.practicum.yandex.interfaces.TaskManager;
-import com.practicum.yandex.services.Managers;
-import com.practicum.yandex.tasks.Task;
-import com.practicum.yandex.tasks.statuses.TaskStatus;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
